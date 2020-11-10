ce15e7d29eb381b66f186d883b21f88a62a71648
Tiedapang <2750428832@qq.com>
2020-11-10 10:49:25 +0800

    [thj] finished practise2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items,sections)=>{
+   let output = [...items];
+    for(let i=0;i<sections.length;i++){
+        output.slice(sections[i].index+i,0,sections.content)
+    }
+    return output;
+}
 export { inject };
